e2031d84bda3dfb3ef1b8fc3d94f33a056c5474d
Kenneth Dait <kendaitdev@icloud.com>
2024-10-13 15:23:53 -0500

    adding work urls

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const puppeteer = require("puppeteer");
 
-const url =
-  "https://sites.google.com/ascension.org/txaus-on-call-schedule/smca";
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
-await page.goto(url);
+async function start(email) {
+  const url =
+    "https://sites.google.com/ascension.org/txaus-on-call-schedule/smca";
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.type("#identifierId", email);
+  await page.click("#identifierNext > div > button");
+  
+  await page.waitForNavigation();
+  await page.type(, email);
+  await page.click("#idSIButton9");
+  
+  await page.waitForNavigation();
+}
+
+start("kpdait@ascension.org");
